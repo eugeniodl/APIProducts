8b50162db7c6a39e6dd107a55f2da6274dac5236
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-06-15 10:11:53 -0600

    Agregando pruebas unitarias

--- UnitTesting/ProductTesting.cs
+++ UnitTesting/ProductTesting.cs
@@ -1,7 +1,9 @@
 using APIProducts.Controllers;
+using APIProducts.Models;
 using APIProducts.Respository;
 using APIProducts.Respository.IRepository;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using Xunit;
 
 namespace UnitTesting
@@ -23,6 +25,54 @@ namespace UnitTesting
             var result = _controller.Get();
 
             Assert.IsType<OkObjectResult>(result);
+        }
+
+
+        [Fact]
+        public void Get_Quantity()
+        {
+            var result = (OkObjectResult)_controller.Get();
+
+            var products = Assert.IsType<List<Product>>(result.Value);
+            Assert.True(products.Count > 0);
+        }
+
+        [Fact]
+        public void GetById_Ok()
+        {
+            int id = 1;
+
+            var result = _controller.GetById(id);
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void GetById_Exists()
+        {
+            // Arrange
+            int id = 1;
+
+            // Act
+            var result = (OkObjectResult)_controller.GetById(id);
+
+            // Assert
+            var product = Assert.IsType<Product>(result?.Value);
+            Assert.True(product != null);
+            Assert.Equal(id, product?.Id);
+        }
+
+        [Fact]
+        public void GetById_NotFound()
+        {
+            // Arrange
+            int id = 11;
+
+            // Act
+            var result = _controller.GetById(id);
+
+            // Assert
+            var product = Assert.IsType<NotFoundResult>(result);
         }
     }
 }
